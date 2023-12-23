d7d8008b206a042b8b41b99502f89b10234bf20d
PhanNgocTuan <csjchoisoojong@gmail.com>
2023-12-23 14:50:25 +0700

    fix 2

---
+++ Lab3/utility/Transport.java
@@ -0,0 +1,36 @@
+package utility;
+import java.util.Objects;
+import core.*;
+
+abstract public class Transport {
+    protected String model;
+    protected Coordinate coord;
+
+    abstract public void move(Coordinate coord);
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Transport transport = (Transport) obj;
+        return model.equals(transport.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model);
+    }
+
+    @Override
+    public String toString() {
+        return "Transport{" +
+                "model='" + model + '\'' +
+                ", abs=" + coord.getAbs() +
+                ", ord=" + coord.getOrd()+
+                '}';
+    }
+}
